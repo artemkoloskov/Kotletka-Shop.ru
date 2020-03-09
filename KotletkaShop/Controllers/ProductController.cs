bb02a346d3158e9c78a8ad0245c86e92bcc37083
Artem Koloskov <artem.koloskov@gmail.com>
2020-03-09 18:34:32 +1000

    Mended front-end collection views to show only published collections. Specific collection also now shows only visible products. Added front-end product view.

---
+++ KotletkaShop/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KotletkaShop.Data;
+using KotletkaShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace KotletkaShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        /// <summary>
+        /// Иницализация страницы со списком всех товаров.
+        /// </summary>
+        public async Task<IActionResult> Index()
+        {
+            // загрузка из контекста списка товаров.
+            List<Product> products = await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductImages)
+                    .ThenInclude(pi => pi.Image)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(products);
+        }
+
+        /// <summary>
+        /// Инициализация представления страницы конкретного товара
+        /// </summary>
+        /// <param name="id">Идентификатор товара</param>
+        public async Task<IActionResult> Product(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            // загрузка коллекции из БД по идентификтору
+            Product product = await _context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductImages)
+                    .ThenInclude(pi => pi.Image)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.ProductID == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
